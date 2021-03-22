18dc37ea99f95c4386a65af37aba287d34786396
Hung Nguyen <hungdhnt@gmail.com>
2021-03-22 11:10:07 +0700

    update

--- Lab3_NCDC_WeatherData/run.sh
+++ Lab3_NCDC_WeatherData/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-3.2.2.jar \
- -file max_temperature_map.py -mapper max_temperature_map.py \
- -file max_temperature_reduce.py -reducer max_temperature_reduce.py \
+ -file mapper.py -mapper mapper.py \
+ -file reducer.py -reducer reducer.py \
  -input /user/hdoop/data/lab3/ncdc \
  -output /user/hdoop/data/lab3/output 
